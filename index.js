0e5fee62fa7f227942d5ae95a895f98581390edc
Omar <otantawy53@gmail.com>
2022-04-24 15:05:11 -0400

    commit tree b0b2545630d45c06fa5bcb162826bab9946b4b2f

--- index.js
+++ index.js
@@ -1,2 +1,96 @@
+// let num1, num2, num3, num4;
 
-// Write your code here
+////******attempt 1******
+let num1 = 31;
+let num2 = 2;
+
+let multiply = (num1 * num2);
+////******attempt 1******
+
+////******attempt 2******
+// let num1 = 31;
+// let num2 = 2;
+//
+// function multiply (num1 , num2){
+//   let result = (num1 * num2);
+//   return result;
+// }
+
+// console.log(multiply(num1 , num2));
+////******attempt 2******
+
+////******attempt 3******
+// let num1 = 31;
+// let num2 = 2;
+//
+// let multiply = function (num1, num2){
+//   return (num1 * num2);
+// }
+//
+// multiply(num1 , num2);
+////******attempt 3******
+
+//I am an idiot -- i was spelling multiply wrong... attempt 1 worked all along **
+
+////******************************* NEXT QUESTION *******************************
+
+////******attempt 1******
+// function random() {
+//   let random = parseInt(Math.random() * 10 + 1);
+//   return random;
+// }
+
+////******attempt 1******
+
+////******attempt 2******
+// function random() {
+//   let number = (Math.random() * 10 + 1);
+//   Number.parseInt(number);
+//   return number;
+// }
+////******attempt 2******
+
+////******attempt 3******
+// function random() {
+//   n = (Math.random() * 100 + 1);
+//   let rndm = parseInt(n);
+//   return rndm;
+// }
+////******attempt 3******
+
+////******attempt 4******
+// function random() {
+//   n = (Math.random() * 100 + 1);
+//   let rndm = parseInt(n);
+//   return rndm;
+// }
+////******attempt 4******
+
+////**********their solution*********************
+const random = Math.floor( Math.random() * 1000 ) + 1;
+////**********their solution*********************
+
+////******************************* NEXT QUESTION *******************************
+
+////******attempt 1******
+// let num3 = 19;
+// let num4 = 5;
+//
+// function mod(x, y){
+//   return x % y;
+// }
+//
+// mod(num3 , num4);
+////******attempt 1******
+
+////******attempt 2******
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+////******attempt 2******
+
+////******************************* NEXT QUESTION *******************************
+
+////******attempt 1******
+let max = Math.max(1, 5, 10, 15, 20)
+////******attempt 1******
